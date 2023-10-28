3aa4ec79c9635f0707173c2a4143c6bb4b5d9d0f
Jena <jenagansivakumar@gmail.com>
2023-10-28 18:17:31 +0200

    starting navbar, will change branch now

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Gi } from "react-icons/gi";
+import { MdOutlineRestaurantMenu } from "react-icons/md";
+
+import images from "../../constants/images";
+import "./Navbar.css";
+
+export default function Navbar() {
+  return (
+    <>
+      <nav className="app__navbar"> Navbar </nav>
+    </>
+  );
+}

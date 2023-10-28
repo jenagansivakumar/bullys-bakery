1bf7c172c49bda7013073a01a227c51f2e14df9d
Jena <jenagansivakumar@gmail.com>
2023-10-28 18:36:46 +0200

    added image to navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,7 +8,37 @@ import "./Navbar.css";
 export default function Navbar() {
   return (
     <>
-      <nav className="app__navbar"> Navbar </nav>
+      <nav className="app__navbar">
+        <div className="app__navbar-logo">
+          <img
+            className="app__navbar-bullysimage"
+            src={images.bullys}
+            alt="app logo"
+          />
+        </div>
+        <ul className="app__navbar-links">
+          <li className="p__opensans">
+            {" "}
+            <a href="#home"> Home</a>
+          </li>
+          <li className="p__opensans">
+            {" "}
+            <a href="#about"> About</a>
+          </li>
+          <li className="p__opensans">
+            {" "}
+            <a href="#menu"> Menu</a>
+          </li>
+          <li className="p__opensans">
+            {" "}
+            <a href="#menu"> Awards</a>
+          </li>
+          <li className="p__opensans">
+            {" "}
+            <a href="#menu"> Contact</a>
+          </li>
+        </ul>
+      </nav>
     </>
   );
 }

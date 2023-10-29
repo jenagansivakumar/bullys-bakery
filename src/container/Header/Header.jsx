773483cee5508568371b624cbd566f6002ee256e
Jena <jenagansivakumar@gmail.com>
2023-10-29 17:56:31 +0100

    creating header. imported SubHeading component to Header

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React from "react";
 
-import './Header.css';
+import "./Header.css";
 
-const Header = () => (
-  <div>
-    Header
-  </div>
-);
+import images from "../../constants/images";
+import { SubHeading } from "../../components";
 
-export default Header;
+export default function Header() {
+  return (
+    <div className="app__header app__wrapper section__padding" id="home">
+      <div className="app__wrapper_info">
+        <SubHeading />
+      </div>
+
+      <div app__wrapper_img></div>
+    </div>
+  );
+}

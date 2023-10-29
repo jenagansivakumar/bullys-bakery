dd879cf57985fe9cee6d74413039354e452e59de
Jena <jenagansivakumar@gmail.com>
2023-10-29 18:42:57 +0100

    creating CSS for app__header .app__header-h1

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -9,9 +9,9 @@ export default function Header() {
   return (
     <div className="app__header app__wrapper section__padding" id="home">
       <div className="app__wrapper_info">
-        <SubHeading />
+        <SubHeading title="Chase the new flavour" />
+        <h1 className="app__header-h1"> Berlin's Favourite Bakery</h1>
       </div>
-
       <div app__wrapper_img></div>
     </div>
   );

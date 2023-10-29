28ef02867af6a3e288bca67d9822194c0f2b6891
Jena <jenagansivakumar@gmail.com>
2023-10-29 19:31:57 +0100

    added welcome image to header, and a short introduction text

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -10,9 +10,17 @@ export default function Header() {
     <div className="app__header app__wrapper section__padding" id="home">
       <div className="app__wrapper_info">
         <SubHeading title="Chase the new flavour" />
-        <h1 className="app__header-h1"> Berlin's Favourite Bakery</h1>
+        <h1 className="app__header-h1"> The key to fine dining </h1>
+        <p className="p__opensans" style={{ margin: "2rem 0" }}>
+          {" "}
+          A stylish Berlin café for amazing coffees, delicious cakes, and lunch
+          menus. Relax in our warm and welcoming atmosphere, perfect for
+          working, reading, or catching up with friends.
+        </p>
+        <button className="custom__button"> Explore Menu</button>
       </div>
       <div app__wrapper_img></div>
+      <img src={images.welcome} alt="welcome_image" />
     </div>
   );
 }

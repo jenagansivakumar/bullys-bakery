ba5ffec66dd0eac86d6dcb5b0d67c34adc083120
Jena <jenagansivakumar@gmail.com>
2023-10-29 15:37:32 +0100

    creating logic for toggleMenu/ set

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { Gi } from "react-icons/gi";
+import { GiHamburgerMenu } from "react-icons/gi";
 import { MdOutlineRestaurantMenu } from "react-icons/md";
-
 import images from "../../constants/images";
 import "./Navbar.css";
+import { useState } from "react";
 
 export default function Navbar() {
+  const [toggleMenu, setToggleMenu] = useState(false);
   return (
     <>
       <nav className="app__navbar">
@@ -38,6 +39,55 @@ export default function Navbar() {
             <a href="#menu"> Contact</a>
           </li>
         </ul>
+        <div className="app__navbar-login">
+          <a href="#login" className="p__opensans">
+            Login / Register
+          </a>
+          <div />
+
+          <a href="/" className="p__opensans">
+            {" "}
+            Book Table{" "}
+          </a>
+        </div>
+        <div className="app__navbar-smallscreen">
+          <GiHamburgerMenu color="#fff" fontSize={27} onClick={() => {}} />
+          <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
+            <MdOutlineRestaurantMenu
+              fontSize={27}
+              className="overlay__close"
+              onClick={() => {}}
+            />
+            <ul className="app__navbar-smallscreen_links">
+              <li>
+                <a href="#home" onClick={() => {}}>
+                  Home
+                </a>
+              </li>
+              <li>
+                <a href="#about" onClick={() => {}}>
+                  About
+                </a>
+              </li>
+              <li>
+                <a href="#menu" onClick={() => {}}>
+                  Menu
+                </a>
+              </li>
+              <li>
+                <a href="#awards" onClick={() => {}}>
+                  Awards
+                </a>
+              </li>
+              <li>
+                <a href="#contact" onClick={() => {}}>
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
+          )}
+        </div>
       </nav>
     </>
   );

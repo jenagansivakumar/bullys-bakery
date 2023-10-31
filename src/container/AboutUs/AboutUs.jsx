3aedbd8792d6ec7ab3ea62dc4439769e97c7c3de
Jena <jenagansivakumar@gmail.com>
2023-10-31 12:26:37 +0100

    added new color var to index.css

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 
 export default function AboutUs() {
-  return <div>Aboutus</div>;
+  return (
+    <div
+      className="app__aboutus app__aboutus-bg flex__center section__padding"
+      id="about"
+    >
+      <div className="app__aboutus-overlay flex__center"> hello </div>
+    </div>
+  );
 }
